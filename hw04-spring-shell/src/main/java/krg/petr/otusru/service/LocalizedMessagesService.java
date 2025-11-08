0ff6f7c7f792708c033833f5e69a77065111670a
petr.krg <petr.krg@mail.ru>
2025-11-09 03:16:47 +0500

    Первая сдача на проверку.

---
+++ hw04-spring-shell/src/main/java/krg/petr/otusru/service/LocalizedMessagesService.java
@@ -0,0 +1,5 @@
+package krg.petr.otusru.service;
+
+public interface LocalizedMessagesService {
+    String getMessage(String code, Object ...args);
+}
---
+++ hw04-spring-shell/src/main/java/krg/petr/otusru/service/TestRunnerService.java
@@ -0,0 +1,8 @@
+package krg.petr.otusru.service;
+
+import krg.petr.otusru.domain.Student;
+import krg.petr.otusru.domain.TestResult;
+
+public interface TestRunnerService {
+    TestResult run(Student student);
+}
